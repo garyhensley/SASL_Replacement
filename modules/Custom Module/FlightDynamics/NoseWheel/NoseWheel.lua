b7a898c2384e1573bf60d8506aec368cb7be5634
Gary Hensley <gary.hensley@gmail.com>
2022-09-25 23:51:04 -0700

    Nosewheel Changes

--- modules/Custom Module/FlightDynamics/NoseWheel/NoseWheel.lua
+++ modules/Custom Module/FlightDynamics/NoseWheel/NoseWheel.lua
@@ -10,12 +10,16 @@ local drf = {
 	yoke		= globalProperty("sim/cockpit2/controls/yoke_roll_ratio"),
 	rudder		= globalProperty("sim/cockpit2/controls/yoke_heading_ratio"),
 
+	left_brake_ratio		= globalProperty("sim/cockpit2/controls/left_brake_ratio"),
+	right_brake_ratio		= globalProperty("sim/cockpit2/controls/right_brake_ratio"),
+
 	steer_override 	= globalProperty("sim/operation/override/override_wheel_steer"),
 	nosewheel 		= globalProperty("sim/flightmodel/parts/tire_steer_cmd[" .. gkh.NoseWheel.Steer .. "]"),
 	steerdeg1 		= globalProperty("sim/aircraft/gear/acf_nw_steerdeg1[" .. gkh.NoseWheel.Steer .. "]"),
 	steerdeg2 		= globalProperty("sim/aircraft/gear/acf_nw_steerdeg2[" .. gkh.NoseWheel.Steer .. "]"),
 	nosewheel_RPM 	= globalProperty("sim/flightmodel2/gear/tire_rotation_speed_rad_sec[" .. gkh.NoseWheel.Steer .. "]"),
 	nosewheel_defl 	= globalProperty("sim/flightmodel2/gear/tire_vertical_deflection_mtr[" .. gkh.NoseWheel.Steer .. "]"),
+
 	frame_time 		= globalProperty("sim/operation/misc/frame_rate_period"),
 	groundspeed 	= globalProperty("sim/flightmodel/position/groundspeed"),
 	
@@ -25,7 +29,6 @@ local nosewheel = {
 	current = 0,
 	defl = 0,
 	turndeg = 0,
-	maxdeg = gkh.NoseWheel.Degree --max(get(drf.steerdeg1),get(drf.steerdeg2)),
 }
 
 
@@ -51,15 +54,21 @@ end
 function update()
 	if get(drf.Towbar) == 0 then  -- no TowBar attached
 		if get(drf.YokeSteer) == 1 then
-			nosewheel.turndeg = get(drf.yoke) * nosewheel.maxdeg
+			nosewheel.turndeg = get(drf.yoke)
 		else
-			nosewheel.turndeg = get(drf.rudder) * nosewheel.maxdeg
+			nosewheel.turndeg = get(drf.rudder)
 		end
+		nosewheel.turndeg = nosewheel.turndeg * get(drf.steerdeg2)
+		if get(drf.groundspeed) < 3 and (get(drf.left_brake_ratio)>0.1 or get(drf.right_brake_ratio)>0.1) then
+			nosewheel.turndeg = nosewheel.turndeg * get(drf.steerdeg1)
+		end
+		
 		
 		-- Real Steering
 		if get(drf.RealSteer)==1 then
 			if get(drf.steer_override) ~= 1 then set(drf.steer_override, 1) end
 			nosewheel.defl = get(drf.nosewheel_defl)
+			
 
 			if nosewheel.defl > 0.0001 then
 				if get(drf.nosewheel_RPM) > 0.05 then
